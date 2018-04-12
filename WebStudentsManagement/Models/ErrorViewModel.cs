6e0cb9d89b1d8a2fb6f3ac3d0a0e9aa8ccc4f9e1
mihai441 <alex.frunza@gmail.com>
2018-04-12 12:43:12 +0300

    asdasda

---
+++ WebStudentsManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebStudentsManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
