269200ba7dc5274fab52854bebf4a7d112d34795
cristi.alexandrescu <necrocris2000@yahoo.com>
2018-04-13 14:12:01 +0300

    better front-end
    13.04 14:11

---
+++ WebStudentsManagement/Models/ActivityViewModels/Activity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStudentsManagement.Models.ManageViewModels
+{
+    public class Activity
+    {
+        public int IdActivity { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Grade { get; set; }
+
+        public int Attendance { get; set; }
+
+        public string StatusMessage { get; set; }
+    }
+}
